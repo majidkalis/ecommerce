f9ddcd34900fd5401360563aeec844cd9143e4e4
asimhameed123 <asimhameed71119@gmail.com>
2023-05-25 20:36:35 +0500

    "new component are created"

---
+++ src/app/shared/right-banner/right-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-banner',
+  templateUrl: './right-banner.component.html',
+  styleUrls: ['./right-banner.component.css']
+})
+export class RightBannerComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LeftPageComponent } from './left-page/left-page.component';
+import { MainBannerComponent } from './main-banner/main-banner.component';
+import { RightBannerComponent } from './right-banner/right-banner.component';
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
-    LeftPageComponent
+    LeftPageComponent,
+    MainBannerComponent,
+    RightBannerComponent
   ],
   imports: [
     CommonModule,
